2730f6316ee89c537745eb710c337eb5507e9a16
Olaniyi Ajayi <ajayiolaniyi8@gmail.com>
2024-01-13 08:08:20 +0200

    passing domy data into template

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Olaniyi Ajayi',
+        'title': 'Blog post 1',
+        'content': 'First post',
+        'date_posted': 'August 27, 2018'
+    },
+
+    {
+        'author': 'Sandra Osas',
+        'title': 'Blog post 2',
+        'content': 'Second post',
+        'date_posted': 'August 29, 2018'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
