b4fbf43ecb4198b00a5f63e790dd0f4856fcd105
Olaniyi Ajayi <ajayiolaniyi8@gmail.com>
2024-01-22 16:06:17 +0200

    completed profile

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,6 +126,8 @@ CRISPY_TEMPLATE_PACK = 'bootstap5'
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
