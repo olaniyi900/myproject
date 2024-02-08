460b7752de15d10f02e9d97c58c45670f06c1fc7
Olaniyi Ajayi <ajayiolaniyi8@gmail.com>
2024-02-08 20:16:43 +0200

    CRUD implementation completed

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,8 @@ from django.views.generic import (
     ListView, 
     DetailView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
     )
 
 # Create your views here.
@@ -39,16 +40,30 @@ class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         return super().form_valid(form)
     
     
-class PostUpdateView(LoginRequiredMixin, CreateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
-    
-    
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    
+    def text_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'})
